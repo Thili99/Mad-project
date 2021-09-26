23d29090556214f453e6d9e66b83ef5cf1211dd8
Thili99 <it20164980@my.sliit.lk>
2021-09-26 23:07:10 +0530

    Product java class added

---
+++ app/src/main/java/com/example/madproject/addProduct.java
@@ -0,0 +1,37 @@
+package com.example.madproject;
+
+public class addProduct {
+
+        private String productName ;
+        private String productPrice ;
+        private String productDescription;
+
+        public addProduct () {
+        }
+
+        public String getProductName () {
+            return productName;
+        }
+
+        public void setProductName ( String productName ) {
+            this.productName = productName;
+        }
+
+        public String getProductPrice () {
+            return productPrice;
+        }
+
+        public void setProductPrice ( String productPrice ) {
+            this.productPrice = productPrice;
+        }
+
+        public String getProductDescription () {
+            return productDescription;
+        }
+
+        public void setProductDescription ( String productDescription ) {
+            this.productDescription = productDescription;
+        }
+    }
+
+
